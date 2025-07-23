4aaf8c05aee9f21e23d245364c5ffdba822cd1ca
Samuel-LY-Wang <samuellywang@gmail.com>
2025-07-23 16:13:41 -0400

    a bit more progress towards ISBN search

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -17,17 +18,24 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    var isbn=req.body.isbn;
-
+    const isbn=req.body.isbn;
+    try {
+        const response = axios.get(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}`);
+        const bookDetails = response.data
+        const title = bookDetails.title;
+        const author = bookDetails.author;
+    }
+    catch (error) {
+        return res.status(500).json({message: `Error fetching book details: ${error}`})
+    }
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    var auth=req.params.author;
+    const auth=req.params.author;
     var num_books=0;
-    
     // iterate over books
   return res.status(300).json({message: "Yet to be implemented"});
 });
