74770bcfa61d6dcd2a9109362eb03887bbb07581
Samuel-LY-Wang <samuellywang@gmail.com>
2025-07-23 16:00:31 -0400

    a small bit more progress I suppose

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,14 +17,18 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    let validbook = {}
+    var isbn=req.body.isbn;
+
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
+    var auth=req.params.author;
+    var num_books=0;
+    
+    // iterate over books
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
