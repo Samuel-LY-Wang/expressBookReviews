5bcf919f0e11fbad46126aba34e6aeeaffd66549
Samuel-LY-Wang <samuellywang@gmail.com>
2025-07-24 12:26:12 -0400

    finalized reading book data, now for registration I guess

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,39 +17,103 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn=req.body.isbn;
+public_users.get('/isbn',async function (req, res) {
+    let isbn=req.query.isbn;
+    if (!isbn) {
+        return res.status(400).json({message: "Please put an ISBN number!"})
+    }
     try {
-        const response = axios.get(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}`);
-        const bookDetails = response.data
-        const title = bookDetails.title;
-        const author = bookDetails.author;
+        let response=await axios.get(
+            `https://openlibrary.org/api/books`,
+            {
+                params: {
+                    bibkeys: `ISBN:${isbn}`,
+                    format: 'json',
+                    jscmd: 'data'
+                }
+            }
+        );
+        const data=response.data
+        const book=data[`ISBN:${isbn}`];
+        if (book) {
+            return res.status(300).json({
+                title: book.title,
+                author: book.authors[0].name || "Unknown"
+            });
+        }
+        else {
+            return res.status(404).json({message: "Book not found!"});
+        }
     }
     catch (error) {
-        return res.status(500).json({message: `Error fetching book details: ${error}`})
+        console.log(error);
+        return res.status(500).json({message: error})
     }
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    const auth=req.params.author;
-    var num_books=0;
-    // iterate over books
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author',function (req, res) {
+    const author=req.query.author;
+    if (!author) {
+        return res.status(400).json({message: "Please put an author!"})
+    }
+    if (author=="Unknown") {
+        return res.status(300).json({message: "Author unknown!"})
+    }
+    for (let i=1; i<=10; i++) {
+        var book=books[i];
+        if (book.author == author) {
+            return res.status(300).json(book);
+        }
+    }
+    return res.status(404).json({message: "Book not found!"});
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title',function (req, res) {
+  const title=req.query.title;
+    for (let i=1; i<=10; i++) {
+        var book=books[i];
+        if (book.title == title) {
+            return res.status(300).json(book);
+        }
+    }
+    return res.status(404).json({message: "Book not found!"});
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review',async function (req, res) {
+    const isbn=req.query.isbn;
+    try {
+        let response=await axios.get(
+            `https://openlibrary.org/api/books`,
+            {
+                params: {
+                    bibkeys: `ISBN:${isbn}`,
+                    format: 'json',
+                    jscmd: 'data'
+                }
+            }
+        );
+        const book=response.data[`ISBN:${isbn}`];
+        if (book) {
+            // console.log({title: book.title, author: book.authors[0].name});
+            for (let i=1; i<=10; i++) {
+                const check_book=books[i];
+                if (book.title==check_book.title && book.authors[0].name==check_book.author) {
+                    return res.status(300).json({reviews: check_book.reviews});
+                }
+            }
+            return res.status(404).json({message: "Book not found in database!"});
+        }
+        else {
+            return res.status(404).json({message: "Book not found!"});
+        }
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({message: error})
+    }
 });
 
 module.exports.general = public_users;
